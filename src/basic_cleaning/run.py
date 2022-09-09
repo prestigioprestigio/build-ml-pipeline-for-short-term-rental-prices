2267acbf3b94f9ef88ec8c45cc621eaec504a67a
prestigioprestigio <prestigioprestigio123456789@gmail.com>
2022-09-09 20:54:44 +0200

    Added geolocation fix

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -36,7 +36,9 @@ def go(args):
     df = df[idx].copy()
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
-    
+    # drop rows in the dataset that are not in the proper geolocation.
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     # Saving file
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
