acb6efe7478a5d7e575232d728a64beee1a1a740
prestigioprestigio <prestigioprestigio123456789@gmail.com>
2022-09-11 11:44:04 +0200

    Added docstrings to data_check.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -64,7 +64,19 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 # Implement here test_row_count and test_price_range   #
 ########################################################
 def test_row_count(data):
+    """Tests for having an adequate amount of data
+    
+    Args:
+        data (dataframe): Input dataframe
+    """
     assert 15000 < data.shape[0] < 1000000
     
 def test_price_range(data, min_price, max_price):
+    """Tests for price values within minimum and maximum expected values
+
+    Args:
+        data (dataframe): Input dataframe
+        min_price (int): Minimum viable price
+        max_price (int): Maximum viable price
+    """
     assert data['price'].between(min_price, max_price).all()
